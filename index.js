37e3286ed0a5eea19922b2810bcc63a7807c8b40
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-06-25 09:24:31 +0200

    return the correct tree, so vendor-addon is recognized

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ function EmberCLISimpleAuth(project) {
   this.name    = 'Ember CLI Simple Auth';
 }
 
+function unwatchedTree(dir) {
+  return {
+    read:    function() { return dir; },
+    cleanup: function() { }
+  };
+}
+
 EmberCLISimpleAuth.prototype.treeFor = function included(name) {
+  var treePath =  path.join('node_modules/ember-cli-simple-auth', name + '-addon');
+
+  if (fs.existsSync(treePath)) {
+    return unwatchedTree(treePath);
+  }
 };
 
 EmberCLISimpleAuth.prototype.included = function included(app) {
