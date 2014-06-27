906dbf1f20190b96c5cd22b7d5ea3d54ca4e5219
Jeff Hertzler <jeffhertzler@gmail.com>
2014-06-27 10:56:08 -0400

    use new import syntax
    fixes #2

--- index.js
+++ index.js
@@ -25,18 +25,20 @@ EmberCLISimpleAuth.prototype.included = function included(app) {
   this.app = app;
 
   this.app.import('vendor/ember-simple-auth/simple-auth.amd.js', {
-    'simple-auth/authenticators/base':                    ['default'],
-    'simple-auth/authorizers/base':                       ['default'],
-    'simple-auth/mixins/application-route-mixin':         ['default'],
-    'simple-auth/mixins/authenticated-route-mixin':       ['default'],
-    'simple-auth/mixins/authentication-controller-mixin': ['default'],
-    'simple-auth/mixins/login-controller-mixin':          ['default'],
-    'simple-auth/stores/base':                            ['default'],
-    'simple-auth/stores/ephemeral':                       ['default'],
-    'simple-auth/stores/local-storage':                   ['default'],
-    'simple-auth/session':                                ['default'],
-    'simple-auth/configuration':                          ['default'],
-    'simple-auth/initializer':                            ['default']
+    exports: {
+      'simple-auth/authenticators/base':                    ['default'],
+      'simple-auth/authorizers/base':                       ['default'],
+      'simple-auth/mixins/application-route-mixin':         ['default'],
+      'simple-auth/mixins/authenticated-route-mixin':       ['default'],
+      'simple-auth/mixins/authentication-controller-mixin': ['default'],
+      'simple-auth/mixins/login-controller-mixin':          ['default'],
+      'simple-auth/stores/base':                            ['default'],
+      'simple-auth/stores/ephemeral':                       ['default'],
+      'simple-auth/stores/local-storage':                   ['default'],
+      'simple-auth/session':                                ['default'],
+      'simple-auth/configuration':                          ['default'],
+      'simple-auth/initializer':                            ['default']
+    }
   });
 };
 
