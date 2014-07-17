9f9258242569627670a12e24b3aa7dfbf6e43106
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-07-17 18:31:30 +0200

    only export the test helpers for test env

--- index.js
+++ index.js
@@ -24,23 +24,27 @@ EmberCLISimpleAuth.prototype.treeFor = function included(name) {
 EmberCLISimpleAuth.prototype.included = function included(app) {
   this.app = app;
 
+  var exports = {
+    'simple-auth/authenticators/base':                    ['default'],
+    'simple-auth/authorizers/base':                       ['default'],
+    'simple-auth/mixins/application-route-mixin':         ['default'],
+    'simple-auth/mixins/authenticated-route-mixin':       ['default'],
+    'simple-auth/mixins/authentication-controller-mixin': ['default'],
+    'simple-auth/mixins/login-controller-mixin':          ['default'],
+    'simple-auth/stores/base':                            ['default'],
+    'simple-auth/stores/ephemeral':                       ['default'],
+    'simple-auth/stores/local-storage':                   ['default'],
+    'simple-auth/session':                                ['default'],
+    'simple-auth/configuration':                          ['default'],
+    'simple-auth/initializer':                            ['default'],
+  };
+  if (this.app.env === 'test') {
+    exports['simple-auth/test-helpers/authenticate-session'] = ['default'];
+    exports['simple-auth/test-helpers/invalidate-session']   = ['default'];
+  }
+
   this.app.import('vendor/ember-simple-auth/simple-auth.amd.js', {
-    exports: {
-      'simple-auth/authenticators/base':                    ['default'],
-      'simple-auth/authorizers/base':                       ['default'],
-      'simple-auth/mixins/application-route-mixin':         ['default'],
-      'simple-auth/mixins/authenticated-route-mixin':       ['default'],
-      'simple-auth/mixins/authentication-controller-mixin': ['default'],
-      'simple-auth/mixins/login-controller-mixin':          ['default'],
-      'simple-auth/stores/base':                            ['default'],
-      'simple-auth/stores/ephemeral':                       ['default'],
-      'simple-auth/stores/local-storage':                   ['default'],
-      'simple-auth/session':                                ['default'],
-      'simple-auth/configuration':                          ['default'],
-      'simple-auth/initializer':                            ['default'],
-      'simple-auth/test-helpers/authenticate-session':      ['default'],
-      'simple-auth/test-helpers/invalidate-session':        ['default']
-    }
+    exports: exports
   });
 };
 
