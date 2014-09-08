4e00b7048688e2fc62ae0d190e1be83abaf2a61b
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-09-08 15:40:55 +0200

    back to original exports

--- index.js
+++ index.js
@@ -10,20 +10,18 @@ module.exports = {
 
     this.app.import(app.bowerDirectory + '/ember-simple-auth/simple-auth.amd.js', {
       exports: {
-        'simple-auth': [
-          'authenticators/base',
-          'authorizers/base',
-          'mixins/application-route-mixin',
-          'mixins/authenticated-route-mixin',
-          'mixins/authentication-controller-mixin',
-          'mixins/login-controller-mixin',
-          'stores/base',
-          'stores/ephemeral',
-          'stores/local-storage',
-          'session',
-          'configuration',
-          'initializer'
-        ]
+        'simple-auth/authenticators/base':                    ['default'],
+        'simple-auth/authorizers/base':                       ['default'],
+        'simple-auth/mixins/application-route-mixin':         ['default'],
+        'simple-auth/mixins/authenticated-route-mixin':       ['default'],
+        'simple-auth/mixins/authentication-controller-mixin': ['default'],
+        'simple-auth/mixins/login-controller-mixin':          ['default'],
+        'simple-auth/stores/base':                            ['default'],
+        'simple-auth/stores/ephemeral':                       ['default'],
+        'simple-auth/stores/local-storage':                   ['default'],
+        'simple-auth/session':                                ['default'],
+        'simple-auth/configuration':                          ['default'],
+        'simple-auth/initializer':                            ['default']
       }
     });
   }
