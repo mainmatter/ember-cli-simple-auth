f20d2a8739f9589701ee65eeade800a5c5089ed3
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-09-08 11:20:56 +0200

    use new Ember CLI addon system

--- index.js
+++ index.js
@@ -1,51 +1,26 @@
-var path = require('path');
-var fs   = require('fs');
+module.exports = {
+  name: 'ember-cli-simple-auth',
 
-function EmberCLISimpleAuth(project) {
-  this.project = project;
-  this.name    = 'Ember CLI Simple Auth';
-}
+  included: function(app) {
+    this._super.included(app);
 
-function unwatchedTree(dir) {
-  return {
-    read:    function() { return dir; },
-    cleanup: function() { }
-  };
-}
+    var exports = ;
 
-EmberCLISimpleAuth.prototype.treeFor = function included(name) {
-  var treePath = path.join('node_modules/ember-cli-simple-auth', name + '-addon');
-
-  if (fs.existsSync(treePath)) {
-    return unwatchedTree(treePath);
+    this.app.import('vendor/ember-simple-auth/simple-auth.amd.js', {
+      exports: {
+        'simple-auth/authenticators/base':                    ['default'],
+        'simple-auth/authorizers/base':                       ['default'],
+        'simple-auth/mixins/application-route-mixin':         ['default'],
+        'simple-auth/mixins/authenticated-route-mixin':       ['default'],
+        'simple-auth/mixins/authentication-controller-mixin': ['default'],
+        'simple-auth/mixins/login-controller-mixin':          ['default'],
+        'simple-auth/stores/base':                            ['default'],
+        'simple-auth/stores/ephemeral':                       ['default'],
+        'simple-auth/stores/local-storage':                   ['default'],
+        'simple-auth/session':                                ['default'],
+        'simple-auth/configuration':                          ['default'],
+        'simple-auth/initializer':                            ['default'],
+      }
+    });
   }
-};
-
-EmberCLISimpleAuth.prototype.included = function included(app) {
-  this.app = app;
-
-  var exports = {
-    'simple-auth/authenticators/base':                    ['default'],
-    'simple-auth/authorizers/base':                       ['default'],
-    'simple-auth/mixins/application-route-mixin':         ['default'],
-    'simple-auth/mixins/authenticated-route-mixin':       ['default'],
-    'simple-auth/mixins/authentication-controller-mixin': ['default'],
-    'simple-auth/mixins/login-controller-mixin':          ['default'],
-    'simple-auth/stores/base':                            ['default'],
-    'simple-auth/stores/ephemeral':                       ['default'],
-    'simple-auth/stores/local-storage':                   ['default'],
-    'simple-auth/session':                                ['default'],
-    'simple-auth/configuration':                          ['default'],
-    'simple-auth/initializer':                            ['default'],
-  };
-  if (this.app.env !== 'production') {
-    exports['simple-auth/test-helpers/authenticate-session'] = ['default'];
-    exports['simple-auth/test-helpers/invalidate-session']   = ['default'];
-  }
-
-  this.app.import('vendor/ember-simple-auth/simple-auth.amd.js', {
-    exports: exports
-  });
-};
-
-module.exports = EmberCLISimpleAuth;
+}
