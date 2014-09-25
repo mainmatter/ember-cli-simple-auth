b4a499741149af9f686e284cd698ef936e9ea3fb
Marco Otte-Witte <marco.otte-witte@simplabs.com>
2014-09-25 23:08:25 +0200

    use ember simple auth from github #next branch for now

--- blueprints/ember-cli-simple-auth/index.js
+++ blueprints/ember-cli-simple-auth/index.js
@@ -3,6 +3,6 @@ module.exports = {
   },
 
   afterInstall: function() {
-    return this.addBowerPackageToProject('ember-simple-auth', '0.6.6');
+    return this.addBowerPackageToProject('https://github.com/simplabs/ember-simple-auth.git#next');
   }
 };
